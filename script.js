0441230449d21d2602a000c42961dc4c267c7dfe
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-30 13:00:45 +0530

    fetch cat

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const cat = document.getElementById("cat");
 const dog = document.getElementById("dog");
-const fox = document.getElementById("fox");
+const cat_btn = document.getElementById("cat_btn");
 const dog_btn = document.getElementById("dog_btn");
-const fox_btn = document.getElementById("fox_btn");
+
+const getRandomCat = async function () {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const data = await res.json();
+  console.log(data);
+  cat.innerHTML = `<img src='${data[0].url}'/>`;
+};
+
+cat_btn.addEventListener("click", getRandomCat);
 
 const getRandomDog = async function () {
   const res = await fetch("https://dog.ceo/api/breeds/image/random");
