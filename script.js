94c372840218f48d8b84bee03db792e42a9b8eda
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-30 12:39:24 +0530

    fetch dog

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const fox = document.getElementById("fox");
 const dog_btn = document.getElementById("dog_btn");
 const fox_btn = document.getElementById("fox_btn");
 
+const getRandomDog = async function () {
+  const res = await fetch("https://dog.ceo/api/breeds/image/random");
+  const data = await res.json();
+  console.log(data);
+  dog.innerHTML = `<img src='${data.message}'/>`;
+};
+
 dog_btn.addEventListener("click", getRandomDog);
-fox_btn.addEventListener("click", getRandomFox);
