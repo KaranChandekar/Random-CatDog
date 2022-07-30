a3e0685a7992132f3733db211583142758d740ff
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-30 11:36:24 +0530

    added event listeners on buttons

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ const dog = document.getElementById("dog");
 const fox = document.getElementById("fox");
 const dog_btn = document.getElementById("dog_btn");
 const fox_btn = document.getElementById("fox_btn");
+
+dog_btn.addEventListener("click", getRandomDog);
+fox_btn.addEventListener("click", getRandomFox);
