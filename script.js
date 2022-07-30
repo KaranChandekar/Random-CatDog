48d7b1b4486a46853e4fbcc8bfb01edacd50c715
Karan Chandekar <karanchandekar431@gmail.com>
2022-07-30 11:34:49 +0530

    grabbed elements to js

---
+++ script.js
@@ -0,0 +1,4 @@
+const dog = document.getElementById("dog");
+const fox = document.getElementById("fox");
+const dog_btn = document.getElementById("dog_btn");
+const fox_btn = document.getElementById("fox_btn");
